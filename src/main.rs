1846ec0af09fac7b482632ebb6260c0e78288787
Nicolas Le Bozec <nicolas.lebozec73@gmail.com>
2026-02-14 03:33:04 +0100

    Add health check and subscription API with basic tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
+use zero2prod::run;
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = std::net::TcpListener::bind("127.0.0.1:8000")?;
+    run(listener)?.await
 }
